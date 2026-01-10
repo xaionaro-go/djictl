7236cf75a62ff47266a2d7f80f44524722864e8e
Dmitrii Okunev <xaionaro@dx.center>
2026-01-10 18:39:18 +0000

    Pass the enable bit to SetFCCEnabled

--- pkg/duml/fcc.go
+++ pkg/duml/fcc.go
@@ -1,13 +1,12 @@
 package duml
 
-// FCC Mode Trigger (Product Shielded Config)
-const (
-	FCCEnablePayload = uint8(0x01)
-)
-
-func NewFCCEnableMessage() *Message {
+func NewFCCEnableMessage(value bool) *Message {
+	payload := uint8(0x00)
+	if value {
+		payload = uint8(0x01)
+	}
 	return &Message{
 		Type:    MessageTypeFCCSupport,
-		Payload: []byte{FCCEnablePayload}, // Simple enable flag
+		Payload: []byte{payload}, // Simple enable flag
 	}
 }
